8beb2a4835e060fd27659b82a86ac64e8e6a16fa
suji321 <sujisasi4@gmail.com>
2022-06-17 16:53:38 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+              child: FlatButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+            child: Text('click me'),
+          )),
+        ),
+      ),
+    );
+  }
+}
